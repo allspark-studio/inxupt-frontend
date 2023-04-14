773f5e49ab592dd409d4ba25d0936b85a351a382
Marstonchan <879560215@qq.com>
2023-04-15 03:10:32 +0800

    chore(*): 初始化项目
    1. 使用 taro 框架
    2. 安装 eslint 相关依赖 TODO: eslint 暂时不生效，原因待排查。

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'plugin:vue/vue3-essential'],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+    '@typescript-eslint',
+  ],
+  rules: {
+    // 防止报错：Component name "index" should always be multi-word.
+    'vue/multi-word-component-names': 'off',
+    'max-len': [100, 'warn'],
+  },
+};
