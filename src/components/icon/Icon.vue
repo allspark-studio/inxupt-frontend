ec449b43c93fa17a1ad638e84fb00101c98ef152
Marstonchan <879560215@qq.com>
2023-04-28 03:30:42 +0800

    feat(icon): 封装 icon 组件

---
+++ src/components/icon/Icon.vue
@@ -0,0 +1,12 @@
+<template>
+  <text :class="['iconfont', `icon-${name}`]" :style="{ fontSize: `${size}px` }"></text>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+defineProps<{
+  size: number;
+  name: string;
+}>();
+</script>
