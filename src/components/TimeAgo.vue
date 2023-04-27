0929fd18abe13d32e7515b1c3f1489ff9c58bf7d
venus <2696651135@qq.com>
2023-04-27 15:13:14 +0800

    修改TimeAgo组件,更改dayjs配置

--- src/components/TimeAgo.vue
+++ src/components/TimeAgo.vue
@@ -2,45 +2,51 @@
   <span>{{ Ago }}</span>
 </template>
 
-<script>
+<script setup>
+import { computed } from "vue"
 import dayjs from "../day/dayjs.config"
 
-export default {
-  name: "TimeAgo",
-  props: {
-    time: {
-      type: [String, Number, Date],
-      required: true,
+const props = defineProps({
+  time: {
+    type: [String, Date, Number],
+    required: true,
+  },
+})
+const time = dayjs(props.time)
+const now = dayjs()
+const standardTimeDay = time.format("YYYY-MM-DD")
+const standardTimeYear = time.format("YYYY")
+dayjs.updateLocale("zh-cn", {
+  relativeTime: {
+    future: "%s 后",
+    past: "%s",
+    s: "刚刚",
+    ss: "刚刚",
+    m: "刚刚",
+    mm: function () {
+      if (now.diff(time, "minute") < 3) return "刚刚"
+      else return now.diff(time, "minute") + "分钟前"
     },
+    h: time.format("今天 HH:mm"),
+    hh: function () {
+      if (now.diff(standardTimeDay, "hour") < 24)
+        return time.format("今天 HH:mm")
+      else return time.format("昨天 HH:mm")
+    },
+    d: time.format("昨天 HH:mm"),
+    dd: function () {
+      if (now.diff(standardTimeDay, "day") < 2) return time.format("昨天 HH:mm")
+      else return time.format("MM.DD")
+    },
+    M: time.format("MM.DD"),
+    MM: function () {
+      if (now.diff(standardTimeYear, "Year") < 1) return time.format("MM.DD")
+      else return time.format("YYYY.MM.DD")
+    },
+    y: time.format("YYYY.MM.DD"),
+    yy: time.format("YYYY.MM.DD"),
   },
-  setup(props) {
-    const time = dayjs(props.time)
-    const now = dayjs()
-    dayjs.updateLocale("zh-cn", {
-      relativeTime: {
-        past: "%s",
-        s: "刚刚",
-        m: "刚刚",
-        mm: function () {
-          if (now.diff(time, "minute") < 3) return "刚刚"
-          else return now.diff(time, "minute") + "分钟前"
-        },
-        h: time.format("今天 HH:mm"),
-        hh: time.format("今天 HH:mm"),
-        d: time.format("昨天 HH:mm"),
-        dd: time.format("MM.DD"),
-        M: time.format("MM.DD"),
-        MM: time.format("MM.DD"),
-        y: time.format("YYYY.MM.DD"),
-        yy: time.format("YYYY.MM.DD"),
-      },
-    })
+})
 
-    const Ago = time.fromNow()
-
-    return {
-      Ago,
-    }
-  },
-}
+const Ago = computed(() => time.fromNow())
 </script>
--- src/day/dayjs.config.ts
+++ src/day/dayjs.config.ts
@@ -3,8 +3,23 @@ import relativeTime from "dayjs/plugin/relativeTime"
 import updateLocale from "dayjs/plugin/updateLocale"
 import 'dayjs/locale/zh-cn'
 
-dayjs.extend(relativeTime)
-dayjs.extend(updateLocale)
-dayjs.locale('zh-cn')
+var config = {
+  thresholds: [
+  { l: "s", r: 1 },
+  { l: "m", r: 1 },
+  { l: "mm", r: 59, d: "minute" },
+  { l: "h", r: 1 },
+  { l: "hh", r: 23, d: "hour" },
+  { l: "d", r: 1 },
+  { l: "dd", r: 29, d: "day" },
+  { l: "M", r: 1 },
+  { l: "MM", r: 11, d: "month" },
+  { l: "y" },
+  { l: "yy", d: "year" },
+  ],
+}
 
+dayjs.extend(updateLocale)
+dayjs.extend(relativeTime, config)
+dayjs.locale('zh-cn')
 export default dayjs
