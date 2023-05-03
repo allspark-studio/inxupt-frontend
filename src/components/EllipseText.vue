698f8dba2ec357c166309b5bd3309d4b1af205df
dengxiaolei123 <1908502449@qq.com>
2023-05-03 20:40:03 +0800

    feat(components):新增了EllipseText组件,更改了nutui配置,修复了样式问题

--- src/components/EllipseText.vue
+++ src/components/EllipseText.vue
@@ -10,16 +10,16 @@
 
 <script setup>
 
-const props = defineProps({
+defineProps({
   text: {
     type: String,
-    default: "NutUI3.0上线后我们研发团队也在不断的优化、测试、使用、迭代 Vue3 的相关组件，但是在跨端小程序的开发过程中，发现没有合适的组件库可以支持多端开发。为了填补这一空白，同时为了优化开发者体验，让 NutUI 能够为更多的开发者带来便利，我们决定在 NutUI 中增加小程序多端适配的能力。"
+    default: 'NutUI3.0上线后我们研发团队也在不断的优化、测试、使用、迭代 Vue3 的相关组件，但是在跨端小程序的开发过程中，发现没有合适的组件库可以支持多端开发。为了填补这一空白，同时为了优化开发者体验，让 NutUI 能够为更多的开发者带来便利，我们决定在 NutUI 中增加小程序多端适配的能力。',
   },
   rows: {
     type: String,
-    default: "3"
-  }
-})
+    default: '3',
+  },
+});
 </script>
 <style lang="scss">
 </style>
