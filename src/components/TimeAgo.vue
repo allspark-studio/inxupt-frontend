fa8afaf0ca35dfe44bbe73a99e58c8a3530c7d3c
venus <2696651135@qq.com>
2023-05-15 14:36:34 +0800

    fix(TimeAgo): 修复TimeAgo组件响应式问题

--- src/components/TimeAgo.vue
+++ src/components/TimeAgo.vue
@@ -4,15 +4,14 @@
 
 <script setup lang="ts">
 import { computed } from 'vue';
-import dayjs from '../day/dayjs.config';
+import dayjs from '~/day/dayjs.config';
 
 const props = defineProps<{
   time: string | Date | number;
 }>();
-const time = dayjs(props.time);
 const now = dayjs();
-const standardTimeDay = time.format('YYYY-MM-DD');
-const standardTimeYear = time.format('YYYY');
+const standardTimeDay = dayjs(props.time).format('YYYY-MM-DD');
+const standardTimeYear = dayjs(props.time).format('YYYY');
 dayjs.updateLocale('zh-cn', {
   relativeTime: {
     future: '%s 后',
@@ -21,28 +20,44 @@ dayjs.updateLocale('zh-cn', {
     ss: '刚刚',
     m: '刚刚',
     mm() {
-      if (now.diff(time, 'minute') < 3) return '刚刚';
-      return `${now.diff(time, 'minute')}分钟前`;
+      if (now.diff(dayjs(props.time), 'minute') < 3) return '刚刚';
+      return `${now.diff(dayjs(props.time), 'minute')}分钟前`;
     },
-    h: time.format('今天 HH:mm'),
+    h: dayjs(props.time).format('今天 HH:mm'),
     hh() {
-      if (now.diff(standardTimeDay, 'hour') < 24) { return time.format('今天 HH:mm'); }
-      return time.format('昨天 HH:mm');
+      if (now.diff(standardTimeDay, 'hour') < 24) {
+        return dayjs(props.time).format('今天 HH:mm');
+      }
+      return dayjs(props.time).format('昨天 HH:mm');
     },
-    d: time.format('昨天 HH:mm'),
+    d: dayjs(props.time).format('昨天 HH:mm'),
     dd() {
-      if (now.diff(standardTimeDay, 'day') < 2) return time.format('昨天 HH:mm');
-      return time.format('MM.DD');
+      if (now.diff(standardTimeDay, 'day') < 2) return dayjs(props.time).format('昨天 HH:mm');
+      return dayjs(props.time).format('MM.DD');
     },
-    M: time.format('MM.DD'),
+    M: dayjs(props.time).format('MM.DD'),
     MM() {
-      if (now.diff(standardTimeYear, 'years') < 1) return time.format('MM.DD');
-      return time.format('YYYY.MM.DD');
+      if (now.diff(standardTimeYear, 'years') < 1) return dayjs(props.time).format('MM.DD');
+      return dayjs(props.time).format('YYYY.MM.DD');
     },
-    y: time.format('YYYY.MM.DD'),
-    yy: time.format('YYYY.MM.DD'),
+    y: dayjs(props.time).format('YYYY.MM.DD'),
+    yy: dayjs(props.time).format('YYYY.MM.DD'),
   },
 });
 
-const Ago = computed(() => time.fromNow());
+const Ago = computed(() => {
+  if (props.time) {
+    try {
+      if (dayjs(props.time).isValid()) {
+        return dayjs(props.time).fromNow();
+      }
+      console.log(`时间校验失败：${props.time} 不是合法的日期时间格式。`);
+      return '';
+    } catch (error) {
+      console.log('时间校验出错：', error.message);
+    }
+  }
+  console.log('传入时间为空');
+  return '';
+});
 </script>
